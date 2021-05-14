fc19220e1f95df432bbd6e2123b75dd797964a61
Patrick Miller <thepdm@gmail.com>
2021-05-14 19:40:00 +0000

    new feature: convert Angular to JSX

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,21 @@
 import Head from 'next/head';
 import styles from '../styles/index.module.css';
-import { compile } from 'handlebars-to-jsx';
+import { compile as compileHandlebarsToJsx } from 'handlebars-to-jsx';
+import { compileAngularToJsx } from "../lib/angular-to-jsx";
 import { useState } from 'react';
 
 export default function Home() {
+  const [compiler, setCompiler] = useState("handlebars");
   const [code, setCode] = useState();
 
   const output = (() => {
     try {
-      return compile(code ?? "");
+      if (compiler === "handlebars") {
+        return compileHandlebarsToJsx(code ?? "");
+      } else {
+        return compileAngularToJsx(code ?? "");
+      }
+
     } catch (err) {
       return String(err);
     }
@@ -22,12 +29,29 @@ export default function Home() {
       </Head>
 
       <div className={styles.container}>
-        <textarea
-          className={styles.left}
-          placeholder="Paste handlebars code here and JSX will be output on the right using jsx-to-handlebars"
-          value={code}
-          onChange={(event) => setCode(event.target.value)}
-        ></textarea>
+        <div className={styles.left} style={{
+          flexDirection: "column",
+        }}>
+          <select style={{
+            display: "block",
+            height: `50px`,
+            width: "100%"
+          }} value={compiler} onChange={event => setCompiler(event.target.value)}>
+            <option value="handlebars">Handlebars to JSX</option>
+            <option value="angular" >Angular to JSX</option>
+          </select>
+          <textarea
+            style={{
+              display: "block",
+              height: `calc(100vh - 50px)`,
+              width: "100%"
+            }}
+            placeholder={compiler === "angular" ? `Paste Angular HTML code here and JSX will be output on the right using custom Angular to JSX code` : `Paste Handlebars code here and JSX will be output on the right using jsx-to-handlebars library`}
+            value={code}
+            onChange={(event) => setCode(event.target.value)}
+          ></textarea>
+        </div>
+
         <textarea
           className={styles.right}
           readOnly
