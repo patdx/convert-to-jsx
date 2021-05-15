6f512fa97affaaae163a543a2a5ca47eb94f0138
Patrick Miller <pamiller.pdx@gmail.com>
2021-05-15 11:42:59 +0900

    apply prettier formatting

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 // angular needs original class names
 module.exports = {
-    // webpack: function (cfg) {
-    //   cfg.plugins = cfg.plugins.filter(plugin => {
-    //     return plugin.constructor.name !== 'UglifyJsPlugin';
-    //   });
-  
-    //   return cfg
-    // }
-  }
+  // webpack: function (cfg) {
+  //   cfg.plugins = cfg.plugins.filter(plugin => {
+  //     return plugin.constructor.name !== 'UglifyJsPlugin';
+  //   });
+  //   return cfg
+  // }
+};
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,19 @@
 import Head from 'next/head';
-// import styles from '../styles/index.module.css';
 import { compile as compileHandlebarsToJsx } from 'handlebars-to-jsx';
-import { compileAngularToJsx } from "../lib/angular-to-jsx";
+import { compileAngularToJsx } from '../lib/angular-to-jsx';
 import { useState } from 'react';
 
 export default function Home() {
-  const [compiler, setCompiler] = useState("handlebars");
+  const [compiler, setCompiler] = useState('handlebars');
   const [code, setCode] = useState();
 
   const output = (() => {
     try {
-      if (compiler === "handlebars") {
-        return compileHandlebarsToJsx(code ?? "");
+      if (compiler === 'handlebars') {
+        return compileHandlebarsToJsx(code ?? '');
       } else {
-        return compileAngularToJsx(code ?? "");
+        return compileAngularToJsx(code ?? '');
       }
-
     } catch (err) {
       return String(err);
     }
@@ -30,12 +28,21 @@ export default function Home() {
 
       <div className="flex flex-col md:flex-row h-screen">
         <div className="flex-1 flex flex-col">
-          <select className="w-full" value={compiler} onChange={event => setCompiler(event.target.value)}>
+          <select
+            className="w-full"
+            value={compiler}
+            onChange={(event) => setCompiler(event.target.value)}
+          >
             <option value="handlebars">Handlebars to JSX</option>
-            <option value="angular" >Angular to JSX</option>
+            <option value="angular">Angular to JSX</option>
           </select>
-          <textarea className="block w-full flex-1 font-mono"
-            placeholder={compiler === "angular" ? `Paste Angular HTML code here and JSX will be output on the right using custom Angular to JSX code` : `Paste Handlebars code here and JSX will be output on the right using jsx-to-handlebars library`}
+          <textarea
+            className="block w-full flex-1 font-mono"
+            placeholder={
+              compiler === 'angular'
+                ? `Paste Angular HTML code here and JSX will be output on the right using custom Angular to JSX code`
+                : `Paste Handlebars code here and JSX will be output on the right using jsx-to-handlebars library`
+            }
             value={code}
             onChange={(event) => setCode(event.target.value)}
           ></textarea>
